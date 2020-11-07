d91c6693a161618e0a07a7d059ee3eafe02a3f7f
Edipo Elwes <edipoelwes2@ufpi.edu.br>
2020-11-07 20:38:32 -0300

    Token em todas as requisiçoes autenticadas

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,16 @@
 import axios from 'axios'
 
-const token = localStorage.getItem('@SonhosDeNinar:token')
-
 const api = axios.create({
   baseURL: 'http://127.0.0.1:8000/api',
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
 })
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('@SonhosDeNinar:token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
+
 
 export default api
